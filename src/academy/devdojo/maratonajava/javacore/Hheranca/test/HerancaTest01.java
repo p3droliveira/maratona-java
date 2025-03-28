2bb4c6ba1f67778a1f415d411d205803431ace99
Pedro Oliveira <p3droliveira95@gmail.com>
2025-03-28 19:06:05 -0300

    Herança pt01.

---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.test;
+
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Endereco;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("0125364");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Antônio Marques");
+        pessoa.setCpf("11111111");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Pedro");
+        funcionario.setCpf("25459666");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(20000);
+        System.out.println("-------------");
+        funcionario.imprime();
+    }
+}
