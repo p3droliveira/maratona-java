5b15cf339790b59c5be594e7cca621959f4e098c
Pedro Oliveira <p3droliveira95@gmail.com>
2025-04-01 22:36:06 -0300

    Herança pt02 (Super).

--- src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -1,7 +1,13 @@
 package academy.devdojo.maratonajava.javacore.Hheranca.dominio;
 
 public class Funcionario extends Pessoa {
+
     private double salario;
+
+    public void imprime(){
+        super.imprime();
+        System.out.println(this.salario);
+    }
 
     public double getSalario() {
         return salario;
